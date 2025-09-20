8d1e93b2b160709bdf4f5f7a8988b1dc63ac8104
keita <keita@asecro.com>
2025-09-20 17:17:24 +0800

    Fix deployment configuration and add GitHub Actions workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Replace '<YOUR_REPOSITORY_NAME>' with the name of your GitHub repository
-  base: '/<YOUR_REPOSITORY_NAME>/',
+  base: '/space_ai_game/',
 })
